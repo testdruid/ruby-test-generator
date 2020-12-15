a107871640e9c5109e4f6a33a25d8ff17a3b09e0
cstruble <cstruble@meteorcomm.com>
2020-12-15 12:52:03 -0800

    Add RubyTestGenerator. Update README.md. Add example model JSON.

--- README.md
+++ README.md
@@ -1,1 +1,198 @@
-# ruby-test-generator
+# Ruby Test Generator
+Ruby Test Generator is a model-based testing (MBT) tool, written in Ruby. It uses GraphWalker to generate test inputs to software. It can also execute test inputs using a Ruby class.
+
+## GraphWalker
+GraphWalker is an open source model-based testing (MBT) tool written in Java. It can be run as a REST service and used to traverse a state machine model describing software behavior to generate test sequences.
+
+## Supported Environments
+These instructions have been tested with Red Hat Enterprise Linux 8 and Ruby 2.5.5. 
+
+## Required Gems
+Ruby Test Generator requires the following Ruby gems: 
+```bash
+gem install rest-client
+```
+
+The example Ruby model class wikipedia_navigation_models.rb requires the following Ruby gems:
+```bash
+gem install watir
+```
+
+## Firefox required
+The example Ruby model class wikipedia_navigation_models.rb requires the Firefox browser.
+
+## Download the GraphWalker CLI
+Ruby Test Generator requires the GraphWalker CLI.
+```bash
+wget https://github.com/GraphWalker/graphwalker-project/releases/download/4.3.0/graphwalker-cli-4.3.0.jar
+```
+
+## Workflow
+* Use a model editor such as Altom Altwalker Model Editor or GraphWalker Studio to create a state machine model of the functionality you want to test. Save the model as a JSON file with an appropriate name.
+* Run the GraphWalker CLI as a REST service.
+* Develop a Ruby class with methods for each element (edge or vertex) in the JSON model to execute the test inputs.
+* Run the Test Generator tool, specifying the path to the .json model file and the .rb model class.
+
+### Run the GraphWalker CLI as a REST Service
+```bash
+java -jar graphwalker-cli-4.3.0.jar --debug all online --service RESTFUL
+```
+You should see output similar to the following:
+```bash
+Dec 11, 2020 4:04:54 PM com.sun.jersey.server.impl.application.WebApplicationImpl _initiate
+INFO: Initiating Jersey application, version 'Jersey: 1.19.4 05/24/2017 03:20 PM'
+Dec 11, 2020 4:04:55 PM org.glassfish.grizzly.http.server.NetworkListener start
+INFO: Started listener bound to [0.0.0.0:8887]
+Dec 11, 2020 4:04:55 PM org.glassfish.grizzly.http.server.HttpServer start
+INFO: [HttpServer] Started.
+Try http://localhost:8887/graphwalker/hasNext or http://localhost:8887/graphwalker/getNext
+Press Control+C to end...
+```
+
+## Display the Ruby Test Generator Help message
+
+```bash
+ruby_test_generator.rb --help
+Usage: ruby_test_generator.rb {options}
+
+    -j, --json [JSON]                {optional} Path to the JSON model file to be loaded into GraphWalker (.json)
+    -u, --url  [URL]                 {optional} Base URL of the GraphWalker REST API
+    -r, --ruby [RUBY]                {optional} Path to the Ruby model class file to be executed (.rb)
+    -s, --save [SAVE]                {optional} Path to the directory of the walk file to be saved
+    -w, --walk [WALK]                {optional} Path to the walk file to be executed (.wlk)
+    -h, --help                       Print this help message
+```
+
+## Load a model file into the GraphWalker REST service, without executing a Ruby model class
+
+To load a JSON model file into the GraphWalker REST service without executing a Ruby model class, run with the --json command line options.
+
+```bash
+./ruby_test_generator.rb --json models/examples/wikipedia_navigation_models.json
+```
+Output may look like the following:
+
+```bash
+Test Generator is using GraphWalker REST API at http://localhost:8887/graphwalker.
+Test Generator has loaded a JSON model into GraphWalker.
+GraphWalker is starting its walk.
+ELEMENT: v_wikipedia_main
+ELEMENT: e_recent
+ELEMENT: v_recent_changes
+ELEMENT: e_portal
+ELEMENT: v_portal_contents
+ELEMENT: e_about
+ELEMENT: v_about
+ELEMENT: e_main
+ELEMENT: v_wikipedia_main
+ELEMENT: e_portal
+ELEMENT: v_portal_contents
+ELEMENT: e_community
+ELEMENT: v_community_portal
+ELEMENT: e_recent
+ELEMENT: v_recent_changes
+ELEMENT: e_about
+ELEMENT: v_about
+ELEMENT: e_portal
+ELEMENT: v_portal_contents
+ELEMENT: e_portal
+ELEMENT: v_portal_contents
+ELEMENT: e_main
+ELEMENT: v_wikipedia_main
+ELEMENT: e_portal
+ELEMENT: v_portal_contents
+ELEMENT: e_community
+ELEMENT: v_community_portal
+ELEMENT: e_community
+ELEMENT: v_community_portal
+ELEMENT: e_main
+ELEMENT: v_wikipedia_main
+ELEMENT: e_about
+ELEMENT: v_about
+ELEMENT: e_community
+ELEMENT: v_community_portal
+ELEMENT: e_main
+ELEMENT: v_wikipedia_main
+ELEMENT: e_main
+ELEMENT: v_wikipedia_main
+ELEMENT: e_portal
+ELEMENT: v_portal_contents
+ELEMENT: e_recent
+ELEMENT: v_recent_changes
+ELEMENT: e_main
+ELEMENT: v_wikipedia_main
+ELEMENT: e_recent
+ELEMENT: v_recent_changes
+ELEMENT: e_community
+ELEMENT: v_community_portal
+ELEMENT: e_portal
+ELEMENT: v_portal_contents
+ELEMENT: e_about
+ELEMENT: v_about
+ELEMENT: e_main
+ELEMENT: v_wikipedia_main
+ELEMENT: e_community
+ELEMENT: v_community_portal
+ELEMENT: e_recent
+ELEMENT: v_recent_changes
+ELEMENT: e_recent
+ELEMENT: v_recent_changes
+ELEMENT: e_about
+ELEMENT: v_about
+ELEMENT: e_recent
+ELEMENT: v_recent_changes
+ELEMENT: e_about
+ELEMENT: v_about
+ELEMENT: e_about
+GraphWalker reached 100% edge coverage. Congratulations!
+GraphWalker walked that model in: 68 steps in 0.638038158416748 seconds.
+
+```
+
+## Load a model file into the GraphWalker REST service, execute a Ruby model class, and save the generated sequence to a walk file
+
+To load a model into GraphWalker REST service, and execute a Ruby model class, run the following command. This command also saves the walk sequence to a file in the specified directory, so it can be re-executed if needed.
+
+```bash
+./ruby_test_generator.rb --json models/examples/wikipedia_navigation_models.json --ruby models/examples/wikipedia_navigation_models.rb  --save /tmp
+```
+
+Output may look like the following:
+
+```bash
+TBD
+```
+
+## Execute a Ruby model class from the sequence in a previously saved walk file
+
+To re-execute a Ruby model class, with a previously saved walk sequence, run the following command.
+
+```bash
+./ruby_test_generator.rb --walk /tmp/TBD.wlk --ruby models/examples/model_example_wikipedia.rb
+```
+
+Output may look like the following:
+
+```bash
+TBD
+```
+
+## Contact
+
+Ruby Test Generator was created by Chris Struble.
+TBD
+
+## References
+The GraphWalker model-based testing tool, by Kristian Karl
+
+http://graphwalker.github.io/
+
+The Altom AltWalker Model Editor
+
+https://gitlab.com/altom/altwalker/model-editor
+
+https://altom.gitlab.io/altwalker/model-editor
+
+General articles about model-based testing, by Harry Robinson
+
+http://www.harryrobinson.net/
---
+++ ruby_test_generator.rb
@@ -0,0 +1,292 @@
+#!/usr/bin/env ruby
+#
+# The Ruby Test Generator is a command line program.
+# It uses GraphWalker to generate test sequences and inputs.
+# It can execute a method in a Ruby model class file for each element.
+# The element names in the model file, and the model class file must match exactly.
+# The Test Generator stops when edge coverage has been reached, or no next element is available.
+
+require "json"
+require "optparse"
+require "rest-client"
+
+module RubyTestGenerator
+
+  # Parse the command line options
+  # @return [Hash] - A hash of the command line options
+  def self.parse_options
+    options = {}
+    built_opts = {}
+
+    option_parser = OptionParser.new  do |opts|
+      opts.banner = "Usage: ruby_test_generator.rb {options}"
+      opts.separator(" ")
+
+      opts.on("-j", "--json [JSON]", String, "{optional} Path to the JSON model file to be loaded into GraphWalker (.json)") do |json|
+        options[:json] = json
+      end
+
+      opts.on("-u", "--url  [URL]", String, "{optional} Base URL of the GraphWalker REST API") do |url|
+        options[:url] = url
+      end
+
+      opts.on("-r", "--ruby [RUBY]", String, "{optional} Path to the Ruby model class file to be executed (.rb)") do |ruby|
+        options[:ruby] = ruby
+      end
+
+      opts.on("-s", "--save [SAVE]", String, "{optional} Path to the directory of the walk file to be saved") do |save|
+        options[:save] = save
+      end
+
+      opts.on("-w", "--walk [WALK]", String, "{optional} Path to the walk file to be executed (.wlk)") do |walk|
+        options[:walk] = walk
+      end
+
+      opts.on("-h", "--help", "Print this help message") do
+        puts opts
+        exit
+      end
+      built_opts = opts
+    end
+
+    option_parser.parse!(options)
+
+    if options.empty?
+      puts built_opts
+      exit
+    end
+
+    return options
+  end
+
+  # Load the Ruby model class file
+  # @param [String] - Path to the Ruby model class file
+  # @raise [Error] - Error if the Ruby model class does not load
+  # @return [Object] - The name of the class in the model class file
+  def self.load_ruby_model_class(model_class_file)
+    model_class_file_full = File.join (model_class_file)
+    load_result = load(model_class_file_full)
+    unless (load_result == true)
+      raise "ERROR: Model class file #{model_class_file} failed to load."
+    end
+
+    # Determine classname from file name
+    model_class_file_basename = File.basename(model_class_file_full,".rb")
+    model_class_name = class_name_from_file_name(model_class_file_basename)
+    Object.const_get(model_class_name)
+  end
+
+  # Load the model file
+  # @param [String] - Path to the model file (*.json)
+  # @param [String] - Base url for the GraphWalker REST API
+  # @raise [Error] - Error if the model file is not found, or does not load into GraphWalker
+  def self.load_model_json(model_json_file, base_url)
+    model_json_file_full = File.join (model_json_file)
+    unless File.exists?(model_json_file_full)
+      raise  "ERROR: Model JSON file #{model_json_file} was not found."
+    end
+
+    gw_load_url = "#{base_url}/load"
+
+    load_response = RestClient.post(gw_load_url, File.new("#{model_json_file_full}").read.to_s, {:content_type => 'text/plain'})
+
+    unless (JSON.parse(load_response)['result'] == "ok")
+      raise "ERROR: Failed to load JSON model file #{model_json_file_full} into GraphWalker at URL #{gw_load_url}"
+    end
+  end
+
+  # Save the walk array to a walk file
+  # @param walk [Array] - An ordered list of element names
+  # @param save_directory [String] -  Path to the directory where the walk file will be saved
+  def self.save_walk_sequence_to_file (walk, save_directory)
+    walk_file_path = File.join(save_directory, "walk-" + Time.now.to_i.to_s + ".wlk")
+    if File.exists?(walk_file_path)
+      raise  "ERROR: Walk file #{walk_file_path} already exists"
+    end
+    File.open(walk_file_path, "w+") do |f|
+      f.puts(walk)
+    end
+    puts "Saved the walk to the file: #{walk_file_path}"
+  end
+
+  # Execute the element method in the model class
+  # @param class_obj [Class] - the model class object
+  # @param element [String] - The name of the element method being called
+  def self.send_model_class_method (class_obj, element)
+    unless class_obj.methods.include? element.to_sym
+      raise "ERROR: Method #{element} not defined in class #{class_obj.name.to_s}"
+    end
+    element_response = class_obj.send(element)
+  end
+
+
+  # Run from a walk file
+  # @param walk_file_path [String] - Path to the walk file to be run
+  def self.run_from_walk_file(options)
+    puts "Test Generator has started to replay the walk file #{options[:walk]}."
+    startTime = Time.now.to_f
+
+    walk_file_path = options[:walk]
+    if(options[:ruby])
+      model_class = load_ruby_model_class(options[:ruby])
+    end
+    File.readlines(walk_file_path).each do |element_name|
+      # output the element name
+      element_method = element_name.strip!
+      puts "ELEMENT: #{element_method}"
+
+      # Call the model element method in the model class
+      if(options[:ruby])
+        element_response = send_model_class_method(model_class, element_method)
+        puts element_response
+        unless (element_response.include?"SUCCESS") || (element_response.include?"PENDING")
+          raise(element_response)
+        end
+      end
+    end
+
+    # Calculate elapsed time
+    endTime = Time.now.to_f
+    elapsedTime = endTime - startTime
+
+    puts "Test Generator replayed the walk file in:  #{elapsedTime} seconds."
+    puts ""
+
+    return "COMPLETED"
+  end
+
+  # Run from GraphWalker REST API
+  # The graphWalker container must be running with a model
+  # @param options [Hash] - the command line options
+  # @raise [Error] - Raise error if GraphWalker does not response as expected
+  def self.run_from_graph_walker (options)
+
+    # Initialize walk array
+    walk = []
+
+    # Default GraphWalker base url to local
+    base_url= "http://localhost:8887/graphwalker"
+
+    if(options[:url])
+      base_url = options[:url]
+    end
+    puts "Test Generator is using GraphWalker REST API at #{base_url}."
+
+    # Load the GraphWalker model
+    if(options[:json])
+      model_json_file_full = File.join (options[:json])
+      load_model_json(options[:json], base_url)
+      puts "Test Generator has loaded a JSON model into GraphWalker."
+    end
+
+    puts "GraphWalker is starting its walk."
+
+    # Initialize walk counter and timer
+    walk_counter = 0
+    startTime = Time.now.to_f
+
+    # Restart the GraphWalker model to its initial state
+    # Raise error if GraphWalker does not respond
+    restart_url = "#{base_url}/restart"
+    restart_response = RestClient.put("#{restart_url}","")
+
+    unless (JSON.parse(restart_response)['result'] == "ok")
+      raise "GraphWalker restart endpoint did not return a result of \"ok\"."
+    end
+
+    # Load the Ruby model class
+    if(options[:ruby])
+      model_class = load_ruby_model_class(options[:ruby])
+    end
+
+    # Request the next element from GraphWalker
+    # Break if edge coverage has been achieved, or if there is no next element
+    # Raise error if GraphWalker does not respond
+    loop do
+      # Get the execution statistics
+      statistics_url = "#{base_url}/getStatistics"
+      statistics_response = RestClient.get("#{statistics_url}")
+
+      # statistics_response = `curl -s #{statistics_url}`
+      unless (JSON.parse(statistics_response)['result'] == "ok")
+        raise "GraphWalker getStatistics endpoint did not return a result of \"ok\"."
+      end
+      if (JSON.parse(statistics_response)['edgeCoverage'].to_i == 100)
+        puts "GraphWalker reached 100% edge coverage. Congratulations!"
+        break
+      end
+
+      # Check that GraphWalker model has a next element
+      # Raise error if GraphWalker does not respond
+      hasNext_url =  "#{base_url}/hasNext"
+      hasNext_response = RestClient.get("#{hasNext_url}")
+      unless (JSON.parse(hasNext_response)['result'] == "ok")
+        raise "GraphWalker hasNext endpoint did not return a result of \"ok\"."
+      end
+      unless (JSON.parse(hasNext_response)['hasNext'] == "true")
+        puts "GraphWalker hasNext endpoint did not return a result of \"true\"."
+        break
+      end
+
+      # Get the next element name
+      # Raise error if GraphWalker does not respond
+      getNext_url =  "#{base_url}/getNext"
+      getNext_response = RestClient.get("#{getNext_url}")
+      unless (JSON.parse(getNext_response)['result'] == "ok")
+        raise "GraphWalker getNext endpoint did not return a result of \"ok\"."
+      end
+      element_name = JSON.parse(getNext_response)['currentElementName']
+
+      # output the element name
+      puts "ELEMENT: #{element_name}"
+
+      # Increment walk counter
+      walk_counter += 1
+
+      # add the element to the walk array
+      if(options[:save])
+        walk << element_name
+      end
+
+      # Call the model element method in the model class
+      if(options[:ruby])
+        element_response = send_model_class_method(model_class, element_name)
+        puts element_response
+        unless (element_response.include?"SUCCESS") || (element_response.include?"PENDING")
+          raise(element_response)
+        end
+      end
+    end
+
+    # Calculate elapsed time
+    endTime = Time.now.to_f
+    elapsedTime = endTime - startTime
+
+    puts "GraphWalker walked that model in: #{walk_counter} steps in #{elapsedTime} seconds."
+
+    # Save the walk array to a walk file
+    if(options[:save])
+      save_walk_sequence_to_file(walk, options[:save])
+    end
+
+    puts ""
+
+    return "COMPLETED"
+  end
+
+  # Return a Ruby class name from a Ruby class file name
+  def self.class_name_from_file_name(file_name)
+    file_name.split('_').map{|e|e.capitalize}.join
+  end
+
+end
+
+if $0 == __FILE__
+    options = RubyTestGenerator.parse_options
+
+    if options[:walk]
+      RubyTestGenerator.run_from_walk_file(options)
+    else
+      RubyTestGenerator.run_from_graph_walker(options)
+    end
+end
